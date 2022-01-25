cb356006474211e6caeacbbab5f6beefca308815
Njagi Mwaniki <njagi@urbanslug.com>
2022-01-25 19:38:12 +0100

    Work on a matrix

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,23 @@ using namespace std;
 
 extern "C" void show_vector(float const *data, std::size_t size);
 
-extern "C" void show_matrix(float const *data, std::size_t nrow, std::size_t ncol);
+extern "C" void show_matrix(float const *data, std::size_t nrow,
+                            std::size_t ncol);
 
 int main() {
   std::vector<float> vec{10, 20, 30};
   std::vector<float> vec2{40, 50, 60};
   std::vector<vector<float>> matrix{vec, vec2, vec};
 
+  // linearize the matrix
+  std::vector<float> matrix2;
+  for (auto it = matrix.begin(); it != matrix.end(); it++) {
+    for (auto t = it->begin(); t != it->end(); t++) {
+      matrix2.push_back(*t);
+    }
+  }
+
   show_matrix(matrix[0].data(), 3, 3);
+  show_matrix(matrix2.data(), 3, 3);
   // show_vector(vec.data(), vec.size());
 }
