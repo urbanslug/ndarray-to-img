929c79ac9c2bec531e6b85fddd2bed67bf2d9a33
Njagi Mwaniki <njagi@urbanslug.com>
2022-01-26 12:27:17 +0100

    Checkpoint: cleanup & have a better interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,5 @@
-use libc::{c_float, c_int, __u8, __u32, size_t};
+use libc::{c_int, __u8, __u32, size_t};
 use std::slice;
-
-#[no_mangle]
-pub extern fn double_input(input: i32) -> i32 {
-    input * 2
-}
-
-#[no_mangle]
-pub extern fn make_vec(input: i32) -> i32 {
-    input * 2
-}
-
-
-#[no_mangle]
-pub extern fn show_vector(data : *const c_float, size : size_t) {
-		let vec = unsafe{std::slice::from_raw_parts(data, size)};
-
-
-
-    for x in vec {
-				println !("{}", x);
-		}
-
-
-}
-
-#[no_mangle]
-pub extern fn show_matrix(data : *const c_float, nrow : size_t, ncol : size_t) {
-		// let vec = unsafe{std::slice::from_raw_parts(data, size)};
-
-		let mut rows: Vec<&[f32]> = Vec::new();
-    for i in 0..nrow as usize {
-        rows.push(unsafe {
-            slice::from_raw_parts(
-                data.offset(i as isize * ncol as isize),
-                ncol as usize
-            )
-        });
-    }
-    let matrix: &[&[f32]] = &rows[..];
-
-    println!("{:#?}", matrix);
-}
-
-#[repr(C)]
-#[derive(Debug)]
-pub struct Cell {
-    pub value: c_int,
-    pub color: (__u8, __u8, __u8, __u8),
-}
-
-#[no_mangle]
-pub extern fn read_cells(data : *const Cell, nrow : size_t, ncol : size_t) {
-		// let vec = unsafe{std::slice::from_raw_parts(data, size)};
-
-		let step = std::mem::size_of::<Cell>();
-
-		eprintln!("rust {}", step);
-
-		let mut rows: Vec<&[Cell]> = Vec::new();
-
-    for i in 0..nrow as usize {
-        rows.push(unsafe {
-            slice::from_raw_parts(
-                data.offset(i as isize * ncol as isize),
-                ncol as usize
-            )
-        });
-    }
-    let matrix: &[&[Cell]] = &rows[..];
-
-    println!("{:#?}", matrix);
-}
 
 #[repr(C)]
 #[derive(Debug)]
@@ -79,22 +7,33 @@ pub struct Position {
 		pub x: __u32,
     pub y: __u32,
 }
+
 #[repr(C)]
 #[derive(Debug)]
-pub struct CellP {
-		pub position: (__u32, __u32),
+pub struct Color {
+		pub red: __u8,
+    pub green: __u8,
+		pub blue: __u8,
+		pub alpha: __u8
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct Cell {
+		pub position: Position,
     pub value: c_int,
-    pub color: (__u8, __u8, __u8, __u8),
+    pub color: Color,
 }
 
 
 #[no_mangle]
-pub extern fn read_cells_with_position(data : *const CellP, size : size_t) {
-		// let vec = unsafe{std::slice::from_raw_parts(data, size)};
-
-		let vec = unsafe{std::slice::from_raw_parts(data, size)};
-
-
+pub extern fn read_cells(
+		data : *const Cell,
+		length : size_t,
+		_nrow: size_t,
+		_ncol: size_t
+) {
+		let vec = unsafe{slice::from_raw_parts(data, length)};
 
     for x in vec {
 				println !("{:?}", x);
--- src/main.cpp
+++ src/main.cpp
@@ -5,110 +5,50 @@
 #include <tuple>
 #include <vector>
 
-using namespace std;
 
-extern "C" void show_vector(float const *data, std::size_t size);
-
-extern "C" void show_matrix(float const *data, std::size_t nrow, std::size_t ncol);
-
-
-
-typedef std::tuple<uint8_t, uint8_t, uint8_t,
-                   uint8_t>
-    color_t;
-
-typedef std::tuple<uint32_t, uint32_t> position_t;
-
-struct cell {
-	int value;
-	// RGB and alpha channel
-	color_t color;
-};
-
-struct pos_t {
+struct position_t {
   uint32_t x;
   uint32_t y;
 
-  pos_t(uint32_t x_pos, uint32_t y_pos) : x(x_pos), y(y_pos) {}
+  position_t(uint32_t x_pos, uint32_t y_pos) : x(x_pos), y(y_pos) {}
 };
 
-struct cell_p {
+struct color_t {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  uint8_t alpha;
+
+  color_t(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+      : red(r), green(g), blue(b), alpha(a) {}
+};
+
+struct cell_t {
 	position_t pos;
   int value;
   // RGB and alpha channel
   color_t color;
 };
 
-extern "C" void read_cells(cell const *cell, std::size_t nrow,
-                            std::size_t ncol);
+extern "C" void read_cells(cell_t const *cell, std::size_t length,
+                           std::size_t nrow, std::size_t ncol);
 
 
+void do_cells() {
+  color_t black (0, 0, 0, 0);
+  position_t up (20, 10);
+  cell_t c1 = cell_t{up, 10, black};
 
-void do_vectors() {
-  std::vector<float> vec{10, 20, 30};
-  std::vector<float> vec2{40, 50, 60};
-  std::vector<vector<float>> matrix{vec, vec2, vec};
+  color_t white (255, 255, 255, 0);
+  position_t down (30, 50);
+  cell_t c2 = cell_t{down, 10, white};
 
-  // linearize the matrix
-  std::vector<float> matrix2;
-  for (auto it = matrix.begin(); it != matrix.end(); it++) {
-    for (auto t = it->begin(); t != it->end(); t++) {
-      matrix2.push_back(*t);
-    }
-  }
+  std::vector<cell_t> v{c1, c2};
 
-  show_matrix(matrix[0].data(), 3, 3);
-  show_matrix(matrix2.data(), 3, 3);
-  // show_vector(vec.data(), vec.size());
-}
-
-void do_cell() {
-  // ---
-  // cell stuff
-  // ---
-  color_t black = std::make_tuple(0, 0, 0, 0);
-  cell c1 = cell{10, black};
-
-  color_t white = std::make_tuple(255, 255, 255, 0);
-  cell c2 = cell{10, white};
-
-  cout << "cpp Size of cell : " << sizeof(cell) << endl;
-
-  std::vector<cell> col1{c1};
-  std::vector<cell> col2{c2};
-
-  std::vector<std::vector<cell>> matrix3{col1, col2};
-
-  // linearize the matrix
-  std::vector<cell> matrix4;
-  for (auto it = matrix3.begin(); it != matrix3.end(); it++) {
-    for (auto t = it->begin(); t != it->end(); t++) {
-      matrix4.push_back(*t);
-    }
-  }
-
-  read_cells(matrix4.data(), 2, 1);
-}
-
-extern "C" void read_cells_with_position(cell_p const *cell, std::size_t size);
-
-void do_cell_with_position() {
-  color_t black = std::make_tuple(0, 0, 0, 0);
-  position_t up = std::make_tuple(20, 10);
-  cell_p c1 = cell_p{up, 10, black};
-
-  color_t white = std::make_tuple(255, 255, 255, 0);
-  position_t down = std::make_tuple(30, 50);
-  cell_p c2 = cell_p{down, 10, white};
-
-  std::vector<cell_p> v{c1, c2};
-
-  read_cells_with_position(v.data(), 2);
+  read_cells(v.data(), 2, 1000, 1000);
 }
 
 int main() {
+	do_cells();
 
-	do_cell_with_position();
-
-	pos_t pos_t(2,3);
 }
